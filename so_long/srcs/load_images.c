a16c396881e1a607ee4d70e9c7ff56c4d369847d
Moe-Salim91156 <msalim@c2r6s1.42amman.com>
2024-12-17 18:59:28 +0300

    final, evaluated

--- so_long/srcs/load_images.c
+++ so_long/srcs/load_images.c
@@ -6,7 +6,7 @@
 /*   By: msalim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 19:09:56 by msalim            #+#    #+#             */
-/*   Updated: 2024/12/15 17:44:51 by msalim           ###   ########.fr       */
+/*   Updated: 2024/12/16 13:53:22 by msalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ t_images	*init_images_textures(void *mlx, t_game *game)
 		free_exit(game);
 	images->height = 64;
 	images->width = 64;
-	images->wall = mlx_xpm_file_to_image(mlx, "./assets/wall64.xpm",
+	images->wall = mlx_xpm_file_to_image(mlx, "./textures/wall64.xpm",
 			&images->width, &images->height);
-	images->floor = mlx_xpm_file_to_image(mlx, "./assets/floor.xpm",
+	images->floor = mlx_xpm_file_to_image(mlx, "./textures/floor.xpm",
 			&images->width, &images->height);
-	images->exit = mlx_xpm_file_to_image(mlx, "./assets/exit.xpm",
+	images->exit = mlx_xpm_file_to_image(mlx, "./textures/exit.xpm",
 			&images->width, &images->height);
-	images->player = mlx_xpm_file_to_image(mlx, "./assets/player.xpm",
+	images->player = mlx_xpm_file_to_image(mlx, "./textures/player.xpm",
 			&images->width, &images->height);
-	images->collectible = mlx_xpm_file_to_image(mlx, "./assets/diamond.xpm",
+	images->collectible = mlx_xpm_file_to_image(mlx, "./textures/diamond.xpm",
 			&images->width, &images->height);
 	return (images);
 }
