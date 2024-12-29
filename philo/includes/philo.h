bc15ed4d24103fc28cf29f167ba4fd16fc468e51
Moe-Salim91156 <msalim@c2r4s13.42amman.com>
2024-12-29 19:55:56 +0300

    beginning of philo

---
+++ philo/includes/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msalim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/29 16:31:53 by msalim            #+#    #+#             */
+/*   Updated: 2024/12/29 16:34:50 by msalim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include  <string.h>
+
+#endif
